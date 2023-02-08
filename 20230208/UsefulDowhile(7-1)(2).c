18f8eb3a59866bd18e16a53d1b47516e2bc88504
siwlxkf <123626009+siwlxkf@users.noreply.github.com>
2023-02-08 16:32:13 +0900

    Create UsefulDowhile(7-1)(2).c

---
+++ 20230208/UsefulDowhile(7-1)(2).c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int total=0, num=1;
+
+	while(num!=0)
+	{
+		printf("정수 입력: ");
+		scanf("%d", &num);
+		total += num;
+	}
+	printf("합계: %d \n", total);
+	return 0;
+}
